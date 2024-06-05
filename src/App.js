d789fe61b2031cb59771164d916a8ebef23e18d7
cristin <christin-antony>
2024-06-05 20:22:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-
 import "./App.css";
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Login from "./components/Login";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
-import Add from "./components/Add"
-
-
+import Add from "./components/Add";
 
 function App() {
- 
   return (
     <div className="App">
-    <BrowserRouter> 
-      <Routes>
-        
-                            <Route path='/' element={<Home />} />
-                            <Route path='/YouTube-clone' element={<Home />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/YouTube-clone-web" element={<Home />} />
 
-                            <Route path='/login' element={<Login />} />
-                            <Route path='/add' element={<Add/>} />
-
-
-    </Routes>
-    </BrowserRouter>  
+          <Route path="/login" element={<Login />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
